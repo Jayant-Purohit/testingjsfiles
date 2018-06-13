e7f3a84c0e0b3dccc39099eeaa679645111e5fff
Jayant Purohit <purohitjay19@gmail.com>
2018-06-13 05:28:40 -0700

    Update outer.js
    added

--- js/outer.js
+++ js/outer.js
@@ -1,6 +1,8 @@
-document.onload = function(e){ 
+window.onload = function(e){ 
    var x = document.getElementsByTagName("TITLE");
-   x.innerHTML = 'this is title from outer js' 
+   x[0].innerHTML = 'this is title from outer js' 
 }
+
+
 
 console.log("hello from outer");
